3046a902e819fff4b0019332487fdac6df054f76
mishapodl <mishapod@gmail.com>
2020-07-28 21:25:22 +0300

    prepare

--- src/components/table/Table.js
+++ src/components/table/Table.js
@@ -1,10 +1,47 @@
 import { ExcelComponents } from '@core/ExcelComponents';
 import { createTable } from '@/components/table/table.tmplate';
+import { $ } from '@core/dom';
 
 export class Table extends ExcelComponents {
    static className = 'excel__table';
 
+   constructor($root) {
+      super($root, {
+         listeners: ['mousedown']
+      });
+   }
+
    toHTML() {
       return createTable(20);
+   }
+
+   onMousedown(event) {
+      if (event.target.dataset.resize) {
+         const $resizer = $(event.target);
+         // const $parent = $resizer.$el.parentNode // bad!
+         // const $parent = $resizer.$el.closest('.column') // better but bad
+         const $parent = $resizer.closest('[data-type="resizable"]');
+         const coords = $parent.getCoords();
+         const type = $resizer.data.resize;
+
+         const cells = this.$root.findAll(`[data-col="${$parent.data.col}"]`);
+
+         document.onmousemove = e => {
+            if (type === 'col') {
+               const delta = e.pageX - coords.right;
+               const value = coords.width + delta;
+               $parent.css({ width: value + 'px' });
+               cells.forEach(el => el.style.width = value + 'px');
+            } else {
+               const delta = e.pageY - coords.bottom;
+               const value = coords.height + delta;
+               $parent.css({ height: value + 'px' });
+            }
+         };
+
+         document.onmouseup = () => {
+            document.onmousemove = null;
+         };
+      }
    }
 }
--- src/components/table/table.tmplate.js
+++ src/components/table/table.tmplate.js
@@ -7,32 +7,34 @@ function toChar(_, index) {
    return String.fromCharCode(CODES.A + index);
 }
 
-function toColumn(col) {
+function toColumn(col, index) {
    return `
-       <div class="column">
-           ${col}
-           <div class="col-resize" data-resize="col"></div>
-       </div>
-   `;
+    <div class="column" data-type="resizable" data-col="${index}">
+      ${col}
+      <div class="col-resize" data-resize="col"></div>
+    </div>
+  `;
 }
 
-function toCell() {
-   return `<div class="cell" contenteditable></div>`;
+function toCell(_, col) {
+   return `
+    <div class="cell" contenteditable data-col="${col}"></div>
+  `;
 }
 
-function createRow(index, row) {
+function createRow(index, content) {
    const resizer = index
       ? '<div class="row-resize" data-resize="row"></div>'
       : '';
 
    return `
-      <div class="row" data-parent="row">
+      <div class="row" data-type="resizable">
         <div class="row-info">
             ${index ? index : ''}
             ${resizer}
         </div>
          <div class="row-data">
-            ${row}
+            ${content}
          </div>
       </div>
    `;
@@ -50,13 +52,13 @@ export function createTable(countRows = 15) {
 
    rows.push(createRow(null, cols));
 
-   const cells = new Array(colsCount)
-      .fill('')
-      .map(toCell) // () => toCell()
-      .join('');
+   for (let i = 0; i < countRows; i++) {
+      const cells = new Array(colsCount)
+         .fill('')
+         .map(toCell)// () => toCell()
+         .join('');
 
-   for (let i = 1; i <= countRows; i++) {
-      rows.push(createRow(i, cells));
+      rows.push(createRow(i + 1, cells));
    }
 
    return rows.join('');
