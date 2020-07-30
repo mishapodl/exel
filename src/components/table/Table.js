f3156fde5092f447776f7be1dd4b8c83ab6b4abd
mishapodl <mishapod@gmail.com>
2020-07-30 12:35:18 +0300

    group select

--- src/components/table/Table.js
+++ src/components/table/Table.js
@@ -1,8 +1,9 @@
 import { ExcelComponents } from '@core/ExcelComponents';
-import { createTable } from '@/components/table/table.tmplate';
-import { shouldResize } from '@/components/table/table.functions';
-import { resizeHandler } from '@/components/table/table.resize';
 import { TableSelection } from '@/components/table/TableSelection';
+import { $ } from '@core/dom';
+import { isCell, shouldResize } from '@/components/table/table.functions';
+import { createTable } from '@/components/table/table.tmplate';
+import { resizeHandler } from '@/components/table/table.resize';
 
 export class Table extends ExcelComponents {
    static className = 'excel__table';
@@ -12,17 +13,14 @@ export class Table extends ExcelComponents {
          listeners: ['mousedown']
       });
    }
-
    prepare() {
-      this.selection = new TableSelection(this.$root);
+      this.selection = new TableSelection();
    }
-
    init() {
       super.init();
 
       this.selection.select(this.$root.find('[data-id="0:0"]'));
    }
-
    toHTML() {
       return createTable(20);
    }
@@ -30,6 +28,18 @@ export class Table extends ExcelComponents {
    onMousedown(event) {
       if (shouldResize(event)) {
          resizeHandler(this.$root, event);
+      } else if (isCell(event)) {
+         this.select(event);
+      }
+   }
+
+   select(event) {
+      const $target = $(event.target);
+
+      if (!event.ctrlKey) {
+         this.selection.select($target);
+      } else {
+         this.selection.selectGroup($target);
       }
    }
 }
--- src/components/table/TableSelection.js
+++ src/components/table/TableSelection.js
@@ -1,16 +1,23 @@
-import { $ } from '@core/dom';
-
 export class TableSelection {
+   static className = 'selected';
+
    constructor() {
       this.group = [];
    }
 
    select($el) {
-      this.group.push($($el));
-      $el.addClass('selected');
+      this.clear();
+      this.group.push($el);
+      $el.addClass(TableSelection.className);
    }
 
-   selectGroup() {
-      console.log('groupSelect');
+   selectGroup($el) {
+      this.group.push($el);
+      $el.addClass(TableSelection.className);
+   }
+
+   clear() {
+      this.group.forEach($el => $el.removeClass(TableSelection.className));
+      this.group = [];
    }
 }
