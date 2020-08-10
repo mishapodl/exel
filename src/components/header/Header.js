c1e801ede5b00d42ca433ed9c07aa1553b67f808
mishapodl <mishapod@gmail.com>
2020-08-10 23:06:46 +0300

    Debounce store

--- src/components/header/Header.js
+++ src/components/header/Header.js
@@ -2,6 +2,7 @@ import { ExcelComponent } from '@core/ExcelComponent';
 import { $ } from '@core/dom';
 import * as actions from '@/redux/actions';
 import { defaultTitle } from '@/constants';
+import { debounce } from '@core/utils';
 
 export class Header extends ExcelComponent {
    static className = 'excel__header';
@@ -13,6 +14,10 @@ export class Header extends ExcelComponent {
          subscribe: ['title'],
          ...options
       });
+   }
+
+   prepare() {
+      this.onInput = debounce(this.onInput, 300);
    }
 
    toHTML() {
--- src/index.js
+++ src/index.js
@@ -6,14 +6,14 @@ import { Formula } from '@/components/formula/Formula';
 import { Table } from '@/components/table/Table';
 import { createStore } from '@core/createStore';
 import { rootReducer } from '@/redux/rootReducer';
-import { storage } from '@core/utils';
+import { debounce, storage } from '@core/utils';
 import { initialState } from '@/redux/initialState';
 
 const store = createStore(rootReducer, initialState);
 
-store.subscribe(state => {
+store.subscribe(debounce(state => {
    storage('excel-state', state);
-});
+}, 1000));
 
 const excel = new Excel('#app', {
    components: [Header, Toolbar, Formula, Table],
