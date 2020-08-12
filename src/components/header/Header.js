167295b233cf2a7c254959567eec897747eab98f
mishapodl <mishapod@gmail.com>
2020-08-12 14:46:20 +0300

    Routing - tables navigation active

--- src/components/header/Header.js
+++ src/components/header/Header.js
@@ -3,6 +3,7 @@ import { $ } from '@core/dom';
 import { changeTitle } from '@/redux/actions';
 import { defaultTitle } from '@/constants';
 import { debounce } from '@core/utils';
+import { ActiveRoute } from '@core/routes/ActiveRoute';
 
 export class Header extends ExcelComponent {
    static className = 'excel__header';
@@ -10,7 +11,7 @@ export class Header extends ExcelComponent {
    constructor($root, options) {
       super($root, {
          name: 'Header',
-         listeners: ['input'],
+         listeners: ['input', 'click'],
          ...options
       });
    }
@@ -22,24 +23,34 @@ export class Header extends ExcelComponent {
    toHTML() {
       const title = this.store.getState().title || defaultTitle;
       return `
-      <input type="text" class="input" value="${title}" />
-
-      <div>
-
-        <div class="button">
-          <i class="material-icons">delete</i>
-        </div>
-
-        <div class="button">
-          <i class="material-icons">exit_to_app</i>
-        </div>
-
-      </div>
-    `;
+         <input type="text" class="input" value="${title}" />
+   
+         <div>
+   
+           <div class="button" data-button="remove">
+              <i class="material-icons" data-button="remove">delete</i>
+           </div>
+   
+           <div class="button" data-button="exit">
+             <i class="material-icons" data-button="exit">exit_to_app</i>
+           </div>
+   
+         </div>
+      `;
    }
 
    onInput(event) {
       const $target = $(event.target);
       this.$dispatch(changeTitle($target.text()));
+   }
+
+   onClick(event) {
+      const data = $(event.target).data.button;
+      if (data === 'remove') {
+         localStorage.removeItem('excel:' + ActiveRoute.param);
+         ActiveRoute.navigate('');
+      } else if (data === 'exit') {
+         ActiveRoute.navigate('');
+      }
    }
 }
